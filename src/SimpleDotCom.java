9e24fa07b35122d7c948067dbf0c7d98f418bab0
TomaszM1992 <tomek.m.osw@gmail.com>
2017-02-23 22:39:22 +0100

    'comment'

---
+++ src/SimpleDotCom.java
@@ -0,0 +1,13 @@
+
+public class SimpleDotCom {
+	int [] locationCells = new int[3];
+	int numOfHits = 0;
+	
+	String checkYourself (String guess){
+		return "hits";
+	}
+	
+	void setLocationCells(int[] loc){
+	
+	}
+}
