6e2d455aae5207ec5a28cad93d206d16c2ca02e4
TomaszM1992 <tomek.m.osw@gmail.com>
2017-02-24 12:21:10 +0100

    'first_working_release'

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -1,13 +1,30 @@
-
 public class SimpleDotCom {
 	int [] locationCells = new int[3];
 	int numOfHits = 0;
+		
 	
-	String checkYourself (String guess){
-		return "hits";
+	void setLocationCells(int[] locs){
+		locationCells = locs;
+	}
+				
+	String checkYourself (String stringGuess){
+		int guess = Integer.parseInt(stringGuess);
+		String result = "miss";
+		
+		for (int cell : locationCells) {
+			if (guess == cell) {
+				result = "hit";
+				numOfHits++;
+				break;
+			}			
+		}
+		
+		if (numOfHits==locationCells.length) {
+			result = "kill";
+		}
+		
+		System.out.println(result);
+		return result;
 	}
 	
-	void setLocationCells(int[] loc){
-	
-	}
 }
--- src/SimpleDotComTestDrive.java
+++ src/SimpleDotComTestDrive.java
@@ -8,17 +8,11 @@ public class SimpleDotComTestDrive {
 	public void test() {
 		SimpleDotCom dot = new SimpleDotCom();
 		int [] locations = {2,3,4};
-		
+		dot.setLocationCells(locations);
 		String userGuess = "2";
 		String result = dot.checkYourself(userGuess);
-		String testResult = "failed";
-		
-		if (result.equals("hit")) {
-			testResult = "passed";
-		}
-		
-		System.out.println(testResult);
-		assertEquals("passed", testResult);
+
+		assertEquals("hit", result);
 	}
 
 }
