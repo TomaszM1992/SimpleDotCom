be155e9d75d524ce55c051e612452d7477041915
TomaszM1992 <tomek.m.osw@gmail.com>
2017-02-26 22:41:11 +0100

    'BugFixSimpleDotComTestDrive'

--- src/SimpleDotComTestDrive.java
+++ src/SimpleDotComTestDrive.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class SimpleDotComTestDrive {
@@ -7,7 +9,13 @@ public class SimpleDotComTestDrive {
 	@Test
 	public void test() {
 		DotCom dot = new DotCom();
-		int [] locations = {2,3,4};
+		Integer intran = new Integer(2);
+		Integer intran2 = new Integer(2+1);
+		Integer intran3 = new Integer(2+2);
+		ArrayList <String> locations = new ArrayList<>();
+		locations.add(intran.toString());
+		locations.add(intran2.toString());
+		locations.add(intran3.toString());
 		dot.setLocationCells(locations);
 		String userGuess = "2";
 		String result = dot.checkYourself(userGuess);
